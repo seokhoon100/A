dbe5f69d16d0e7e3518054b0f8d157e2c3709ed7
seokhoon100 <100seokoon@gmail.com>
2024-10-11 10:42:38 +0900

    v1.0 git

--- main.go
+++ main.go
@@ -1,3 +1,4 @@
+"""
 package main
 
 import (
@@ -10,3 +11,35 @@ func main() {
 	var month int = int(now.Month())
 	fmt.Println(month)
 }
+"""
+"""
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	var now time.Time = time.now
+	var time int = int(now.Time())
+	fmt.Printf("now time : %dyear %dmonth %dday\n", now.Year(), int(now.Month()), now.Day())
+	fmt.Printf("now time : %dhour %dminute %dsecond\n", now.Hour(), now.Minute(), now.Second())
+}
+"""
+
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	broken := "G# r#cks!"
+	replace := strings.NewReplacer("#", "o")
+	fixed := replacer.Replace(broken)
+	fmt.Printf(fixed)
+}
+
+
